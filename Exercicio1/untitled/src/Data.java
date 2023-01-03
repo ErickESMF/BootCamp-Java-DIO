9efd72913de2a03adc7d433aa844fc9175e78843
ErickESMF <feitosaesmf@gmail.com>
2023-01-03 16:09:28 -0300

    Atualização do titulo

--- Exercicio1/untitled/src/Data.java
+++ Exercicio1/untitled/src/Data.java
@@ -5,7 +5,7 @@ public class Data {
     Date dataHoraAtual = new Date();
     String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
     String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
+    int hora2 = Integer.parseInt(hora);
 
     public void diaDeHoje(){
         System.out.println("Agora são " + hora +" do dia " + data);
